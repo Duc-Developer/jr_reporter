5d60970d46efb51de8493ad10738584af5865809
David Chan <mrtranduc1994@outlook.com>
2024-11-06 22:42:44 +0700

    hot fix domain path when build

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jr_reporter/',
   plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
   }
 })
